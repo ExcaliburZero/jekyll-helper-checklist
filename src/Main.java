eee4cb5ac3e79ee4c897c64909de79c73e9e492d
ExcaliburZero <cwellsny@nycap.rr.com>
2015-08-13 00:50:07 -0400

    Added main file with some basic code
    Added the main file of the program. Added some code for the intro section of the program, and a function to create dividers.

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+
+    /*
+     * The main function of the program.
+     *
+     * @param The arguements that are given when running the program.
+     *
+     * @return Returns 0 when the function completes
+     */
+    public static int main(String[] args) {
+        // Intro section
+        printDivider();
+        System.out.println("Jekyll Helper release checklist");
+        printDivider();
+
+        // Return a value to show that the function has finished running
+        return 0;
+    }
+
+    /*
+     * Function to print a divider across the terminal window
+     *
+     * @return Returns 0 if the function suceedes, and 1 if the function fails.
+     */
+    private static int printDivider() {
+        int counter = 0;
+        int number_of_characters = 40;
+        while(counter < number_of_characters) {
+            System.out.printf("-");
+            counter++;
+        }
+        System.out.printf("\n");
+
+        // Check to make sure the function ran correctly
+        if(counter == number_of_characters)
+            return 0;
+        else
+            return 1;
+    }
+
+}
